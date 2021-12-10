c62a3cfb5a78a6348d47495c5992afcca75b51e9
mnbvmnbv2 <ferdinand@ytteborg.com>
2021-12-10 01:29:32 +0100

    Fix the cost of tiers in tier_to_txt

--- functions.py
+++ functions.py
@@ -26,8 +26,7 @@ def modifs_to_txt(lis):
 
 
 def tier_to_txt(tier):
-    txt = '    %(tier)s = {\n        upgrade_time = {\n            months = %(time)d\n        }\n        province_modifiers = {\n%(prov)s        }\n        area_modifier = {\n%(area)s        }\n        country_modifiers = {\n%(coun)s        }\n        on_upgraded = {\n%(upgr)s        }\n    }' % {
-        'tier': tier['tier'], 'time': tier['upgrade_time'], 'cost': tier['upgrade_cost'], 'prov': modifs_to_txt(tier['province_mods']), 'area': modifs_to_txt(tier['area_mods']), 'coun': modifs_to_txt(tier['country_mods']), 'upgr': modifs_to_txt(tier['province_mods'])}
+    txt = '    %(tier)s = {\n        upgrade_time = {\n            months = %(time)d\n        }\n        cost_to_upgrade = {\n            factor = %(cost)d\n        }\n        province_modifiers = {\n%(prov)s        }\n        area_modifier = {\n%(area)s        }\n        country_modifiers = {\n%(coun)s        }\n        on_upgraded = {\n%(upgr)s        }\n    }' % {'tier': tier['tier'], 'time': tier['upgrade_time'], 'cost': tier['upgrade_cost'], 'prov': modifs_to_txt(tier['province_mods']), 'area': modifs_to_txt(tier['area_mods']), 'coun': modifs_to_txt(tier['country_mods']), 'upgr': modifs_to_txt(tier['province_mods'])}
     return txt
 
 
@@ -35,7 +34,7 @@ def to_txt(monum):
     txt = '%(name)s = {\n    start = %(prov)d\n    date = 1444.01.01\n    build_cost = 0\n    can_be_moved = no\n    move_days_per_unit_distance = 1\n    starting_tier = %(start_tier)d\n    type = monument\n    time = {\n        months = 0\n    }\n    build_trigger = {\n    }\n    can_use_modifiers_trigger = {\n    }\n    can_upgrade_trigger = {\n    }\n    keep_trigger = {\n    }' % {
         "name": monum['name'], "prov": monum['province'], 'start_tier': monum['start_tier']}
     txt += '\n'
-    txt += '    tier_0 = {\n        upgrade_time = {\n            months = 0\n        }\n        cost_to_upgrade = {\n            factor = %(cost)d\n        }\n        province_modifiers = {\n        }\n        area_modifier = {\n        }\n        country_modifiers = {\n        }\n        on_upgraded = {\n        }\n    }'
+    txt += '    tier_0 = {\n        upgrade_time = {\n            months = 0\n        }\n        cost_to_upgrade = {\n            factor = 0\n        }\n        province_modifiers = {\n        }\n        area_modifier = {\n        }\n        country_modifiers = {\n        }\n        on_upgraded = {\n        }\n    }'
     txt += '\n%(tier1)s' % {'tier1': tier_to_txt(monum['tier1'])}
     txt += '\n%(tier2)s' % {'tier2': tier_to_txt(monum['tier2'])}
     txt += '\n%(tier3)s' % {'tier3': tier_to_txt(monum['tier3'])}
